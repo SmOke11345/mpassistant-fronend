1d309d8f36d303c4c7fd32e2341fb7ae30bea0b0
Artyom <Artyom.lymar@bk.ru>
2023-10-20 15:30:11 +0600

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { AsyncPipe, NgIf, NgOptimizedImage } from '@angular/common';
+import { HeaderComponent } from './components/Header/header.component';
+import { FooterComponent } from './components/Footer/footer.component';
+import { RegistrationComponent } from './pages/register/registration.component';
 
 @Component({
-  standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+    standalone: true,
+    imports: [
+        RouterModule,
+        NgOptimizedImage,
+        HeaderComponent,
+        FooterComponent,
+        RegistrationComponent,
+        AsyncPipe,
+        NgIf,
+    ],
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'mpassistant';
-}
+export class AppComponent {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Route } from '@angular/router';
+import { RegistrationComponent } from './pages/register/registration.component';
+import { HomeComponent } from './pages/home/home.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    {
+        path: '',
+        component: HomeComponent,
+    },
+    {
+        path: 'register',
+        component: RegistrationComponent,
+    },
+];
